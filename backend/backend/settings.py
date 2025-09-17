358fba536d390dcc7e3acffcf5a48226d7880228
MrMobbi <julliatmathias06@gmail.com>
2025-09-17 15:22:20 +0200

    env deleted

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'rest_framework',
+    'drf-yasg'
 ]
 
 MIDDLEWARE = [
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from rest_framework import permission
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
